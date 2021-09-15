367ef124f595e1417a2046a532ccc23386f525e3
lehuy-dev <lehuy.developer@gmail.com>
2021-09-15 17:07:21 +0700

    create new project

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/client/MainControler.java
@@ -0,0 +1,13 @@
+package com.shopme.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainControler {
+
+	@GetMapping(value = "/")
+	public String example() {
+		return "index";
+	}
+}
